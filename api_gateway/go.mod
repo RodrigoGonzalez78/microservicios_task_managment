534eb8c23f40bbc7ab5cf167c06e3a7dcf0ed449
RodrigoGonzalez78 <61030017+RodrigoGonzalez78@users.noreply.github.com>
2023-10-31 16:43:00 -0300

    api_gateway

---
+++ api_gateway/go.mod
@@ -0,0 +1,5 @@
+module github.com/RodrigoGonzalez78/api_gateway
+
+go 1.21.1
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ api_gateway/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// Crear el enrutador para el API Gateway
+	router := mux.NewRouter()
+
+	// Ruta para el microservicio de usuarios
+	usersProxy := NewReverseProxy("http://localhost:3000")
+	router.PathPrefix("/users/").Handler(usersProxy)
+
+	// Ruta para el microservicio de tareas
+	tasksProxy := NewReverseProxy("http://localhost:4000")
+	router.PathPrefix("/tasks/").Handler(tasksProxy)
+
+	// Configurar el servidor HTTP
+	server := &http.Server{
+		Addr:    ":8080", // Puerto en el que escuchará el API Gateway
+		Handler: router,
+	}
+
+	fmt.Println("API Gateway en ejecución en el puerto 8080...")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// NewReverseProxy crea un proxy inverso para redirigir solicitudes a un servicio en un puerto determinado
+func NewReverseProxy(target string) *httputil.ReverseProxy {
+	url, err := url.Parse(target)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(url)
+
+	return proxy
+}
