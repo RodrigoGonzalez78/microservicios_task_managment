4615d3413534d82a13692115e632e1ba03affcb0
RodrigoGonzalez78 <61030017+RodrigoGonzalez78@users.noreply.github.com>
2023-11-18 01:49:25 -0300

    adicionales

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
-# microservicios_task_managment
+# Comandos de ejemplos para el despliegue 
+
+## Crear y corre un contenedor docker de las apliciones
+
+### Estos comandos deben ejecutarse en la ruta de cada uno
+```console
+docker build -t tasks-management-app .
+
+docker run -p 3000:3000 tasks-management-app
+```
+
+## Crear y correr un contenedor docker de posgret
+
+```console
+docker run -d --name postgres-tasks-management-app -p 5432:5432 -e POSTGRES_PASSWORD=12345678 -e POSTGRES_DB=taskMicro postgres:latest
+```
+
+
+## Exporter para La base de datos de users
+
+```console
+sudo docker run -d \
+  --name postgres_exporter_users \
+  -p 9187:9187 \
+  -e DATA_SOURCE_NAME="host=192.168.0.168 port=5435 user=postgres password=12345678 dbname=usuarios sslmode=disable" \
+  wrouesnel/postgres_exporter
+```
+
+
+
+## Exporter para la base de datos tasks
+
+```console
+sudo docker run -d \
+  --name postgres_exporter_tasks \
+  -p 9188:9187 \
+  -e DATA_SOURCE_NAME="host=192.168.0.168 port=5432 user=postgres password=12345678 dbname=taskMicro sslmode=disable" \
+  wrouesnel/postgres_exporter
+```
+
+## Cargar y lanzar prometheus
+
+### Recordar usar una configuracion adecuada para su entorno
+```console
+docker run -d --name mi-prometheus-container -p 9090:9090 -v "$PWD/prometheus-config:/etc/prometheus" prom/prometheus --config.file=/etc/prometheus/prometheus.yml
+```
+
+## Cargar y lanzar grafana
+
+```console
+docker run -d --name mi-grafana-container -p 3000:3000 grafana/grafana
+```
+
+
+## Comandos de ayuda
+
+Para saber  todo los contenedores que tenemos
+
+```console
+docker ps -a
+```
+
+Para saber el consumo de recursos de los contedores
+
+```console
+docker stats 
+```
+
+Para saber la direccion ip de un contenedor si es nesario
+
+
+```console
+sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' task-manager-postgres
+```
--- api_gateway/main.go
+++ api_gateway/main.go
@@ -8,6 +8,18 @@ import (
 	"net/url"
 
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	requestCount = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests.",
+		},
+		[]string{"handler", "method", "path"},
+	)
 )
 
 func main() {
@@ -15,12 +27,18 @@ func main() {
 	router := mux.NewRouter()
 
 	// Ruta para el microservicio de usuarios
-	usersProxy := NewReverseProxy("http://172.17.0.5:5000")
+	usersProxy := NewReverseProxy("http://172.17.0.5:5000", "users", "/users")
 	router.PathPrefix("/users").Handler(usersProxy)
 
 	// Ruta para el microservicio de tareas
-	tasksProxy := NewReverseProxy("http://172.17.0.4:4000")
+	tasksProxy := NewReverseProxy("http://172.17.0.4:4000", "tasks", "/tasks")
 	router.PathPrefix("/tasks").Handler(tasksProxy)
+
+	// Registra el contador Prometheus
+	prometheus.MustRegister(requestCount)
+
+	// Configurar el servidor HTTP para métricas Prometheus
+	router.Handle("/metrics", promhttp.Handler())
 
 	// Configurar el servidor HTTP
 	server := &http.Server{
@@ -35,13 +53,24 @@ func main() {
 }
 
 // NewReverseProxy crea un proxy inverso para redirigir solicitudes a un servicio en un puerto determinado
-func NewReverseProxy(target string) *httputil.ReverseProxy {
+func NewReverseProxy(target string, handlerName string, path string) *httputil.ReverseProxy {
 	url, err := url.Parse(target)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	proxy := httputil.NewSingleHostReverseProxy(url)
+
+	// Configura el contador Prometheus para esta ruta
+	proxy.Director = func(req *http.Request) {
+		req.URL.Host = url.Host
+		req.URL.Scheme = url.Scheme
+		req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
+
+		// Incrementa el contador Prometheus con la información de la ruta
+		requestCount.WithLabelValues(handlerName, req.Method, path).Inc()
+	}
 
 	return proxy
 }
